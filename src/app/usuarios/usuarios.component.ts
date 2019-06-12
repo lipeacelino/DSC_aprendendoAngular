54650049e985080e119f50932b25f003b7472eb9
Felipe <felipe.acelino@dce.ufpb.br>
2019-06-12 11:06:42 -0300

    dividindo componentes

---
+++ src/app/usuarios/usuarios.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuarios',
+  templateUrl: './usuarios.component.html',
+  styleUrls: ['./usuarios.component.css']
+})
+export class UsuariosComponent implements OnInit {
+
+  names: string [];
+
+  constructor() { 
+    this.names = ["Mary", "Joe", "Bill", "Anna"];
+  }
+
+  ngOnInit() {
+  }
+
+}
